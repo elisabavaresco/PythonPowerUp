994bcb2f62526fcb46dd128341f14f3c20117299
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-04-09 14:59:39 -0300

    adicionando um arquivo de anotações para deixar o arquivo codigo.py mais limpo

---
+++ anotacoes.py
@@ -0,0 +1,17 @@
+""""
+Antes de começar qualquer projeto, é importante entender qual seria o passo a passo manual que seria executado, qual a lógica que ele deve execuitar, para depois traduzir isso para código. 
+Este é um projeto de automação de tarefas, mais especificamente, automação de cadastro de produtos.
+
+Lógica:
+Passo 1: Entrar no sistema da empresa - https://dlp.hashtagtreinamentos.com/python/intensivao/login
+Passo 2: Fazer login
+Passo 3: Importar a base de dados
+Passo 4: Cadastrar 1 produto
+Passo 5: Repetir para todos os produtos
+
+Bibliotecas:
+pyautogui -> uma das bibliotecas que vamos utilizar nesse projeto é a 'pyautogui', que faz automações com Python, permitindo que você controle o teclado, mause e tela usando Python
+    pyautogui.click -> clicar em algum lugar
+    pyautogui.press -> apertar 1 tecla
+    pyautogui.write -> escrever um texto
+"""
