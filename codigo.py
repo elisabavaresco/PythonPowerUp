5fb539181cd22dd5fed6827e71e6e3d7418793c4
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-04-09 16:40:56 -0300

    finalizando a automação para logar completamente no site da empresa

--- codigo.py
+++ codigo.py
@@ -18,6 +18,15 @@ time.sleep(3)
 
 # Passo 2: Fazer login
 # clicar no campo de email
-# posição do compo de email na tela: x=2004, y=425
+# posição do campo de email na tela: x=2004, y=425
 pyautogui.click(2004, 425)
+# digitar no campo de email
 pyautogui.write('exemplo@gmail.com')
+
+# clicar no campo de senha
+# posição do campo de senha na tela: x=1993, y=519
+# pyautogui.click(1993, 519)
+pyautogui.press('tab')
+# digitar no campo de senha
+pyautogui.write('exemplosenha')
+pyautogui.press('enter')
