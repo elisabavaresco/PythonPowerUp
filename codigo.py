c3d030e782a28203b0430f52ebf20375c7151b71
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2025-04-09 15:53:42 -0300

    criando automação para entrar no site da empresa

--- codigo.py
+++ codigo.py
@@ -5,3 +5,6 @@ pyautogui.hotkey('command','space')
 pyautogui.write('safari')
 pyautogui.press('enter')
 
+# entrar no sistema da empresa
+pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
+pyautogui.press('enter')
